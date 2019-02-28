cdc0b79b2e63f969106a02d24acd8c464168540f
Joseph Nguyen <jnguyen2@fandm.edu>
2019-02-28 17:31:14 -0500

    Stub WordGame model

---
+++ app/src/main/java/com/example/mobileapp/wordly/WordGame.java
@@ -0,0 +1,14 @@
+//
+// WordGame.java
+// Joseph Nguyen
+//
+package com.example.mobileapp.wordly;
+
+import java.util.ArrayList;
+
+public class WordGame {
+    private ArrayList<GraphNode<String>> path;
+
+    public WordGame() {
+    }
+}
