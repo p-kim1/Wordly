be29b0ac39e4dded7e500fb4c1d909cc03aa171e
Joseph Nguyen <jnguyen2@fandm.edu>
2019-02-28 16:24:44 -0500

    Initial commit for graph files

---
+++ app/src/main/java/com/example/mobileapp/wordly/Graph.java
@@ -0,0 +1,126 @@
+//
+// Graph.java
+// Joseph Nguyen
+//
+package com.example.mobileapp.wordly;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.ArrayList;
+
+class GraphNode<T> {
+	private T value;
+
+	// hashset for quick checking of neighbors
+	private HashSet<T> uniqueNeighborValues = new HashSet<>();
+	private ArrayList<GraphNode<T>> neighbors = new ArrayList<>();
+
+	GraphNode(T value) {
+		this.value = value;
+	}
+
+	T getValue() {
+		return value;
+	}
+
+	ArrayList<GraphNode<T>> getNeighbors() {
+		return neighbors;
+	}
+
+	void addNeighbor(GraphNode<T> toNode) {
+		neighbors.add(toNode);
+		uniqueNeighborValues.add(toNode.getValue());
+	}
+
+	GraphNode<T> getRandomNeighbor() {
+		Random generator = new Random();
+		int randomIndex = generator.nextInt(neighbors.size());
+		return neighbors.get(randomIndex);
+	}
+
+	boolean isNeighbor(GraphNode<T> node) {
+		return uniqueNeighborValues.contains(node.getValue());
+	}
+
+	public String toString() {
+		return value.toString();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+
+		if (!(object instanceof GraphNode)) {
+			return false;
+		}
+
+		return this.getValue() == ((GraphNode) object).getValue();
+	}
+}
+
+public class Graph<T> {
+	protected HashMap<T, GraphNode<T>> valueMap = new HashMap<>();
+	protected ArrayList<GraphNode<T>> vertices = new ArrayList<>();
+
+	public ArrayList<GraphNode<T>>  getVertices() {
+		return vertices;
+	}
+
+	public void addEdge(T fromValue, T toValue) {
+		GraphNode<T> fromNode = getVertexWithValue(fromValue);
+		GraphNode<T> toNode = getVertexWithValue(toValue);
+		fromNode.addNeighbor(toNode);
+	}
+
+	public void addVertex(T value) {
+		GraphNode<T> newNode = new GraphNode<>(value);
+		vertices.add(newNode);
+		valueMap.put(value, newNode);
+	}
+
+	public boolean isEmpty() {
+		return vertices.size() == 0;
+	}
+
+	public GraphNode<T> getVertexWithValue(T value) {
+		return valueMap.get(value);
+	}
+
+	public GraphNode<T> getRandomVertex() {
+		if (isEmpty()) {
+			return null;
+		}
+
+		Random generator = new Random();
+		int randomIndex = generator.nextInt(vertices.size());
+		return vertices.get(randomIndex);
+	}
+
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (GraphNode<T> vertex : vertices) {
+			stringBuilder.append(vertex.toString());
+			stringBuilder.append(": ");
+			stringBuilder.append(vertex.getNeighbors().toString());
+			stringBuilder.append('\n');
+		}
+		return stringBuilder.toString();
+	}
+
+	// removes vertices with no neighbors
+	protected void prune() {
+		int i = 0;
+		while (i < vertices.size()) {
+			GraphNode<T> vertex = vertices.get(i);
+			if (vertex.getNeighbors().size() == 0) {
+				vertices.remove(vertex);
+				i--;
+			}
+			i++;
+		}
+	}
+
+}
---
+++ app/src/main/java/com/example/mobileapp/wordly/WordGraph.java
@@ -0,0 +1,161 @@
+//
+// WordGraph.java
+// Joseph Nguyen
+//
+package com.example.mobileapp.wordly;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class WordGraph extends Graph<String> {
+	private void restoreGraphFromFile(File file) {
+		try {
+		    BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
+
+			// read the file line by line
+			String line = reader.readLine();
+			while (line != null && line.length() > 0) {
+				// replace symbols
+				line = line.replaceAll("[\\[:,\\]]", "");
+
+				// split strings on whitespace; first word is vertex of focus, other words are edges
+				String[] words = line.split("\\s+");
+
+				GraphNode<String> newWord = new GraphNode<>(words[0]);
+				for (int i = 1; i < words.length; i++) {
+					GraphNode<String> curr = getVertexWithValue(words[i]);
+
+					// if the vertex doesnt exist
+					if (curr == null) {
+						curr = new GraphNode<>(words[i]);
+						valueMap.put(curr.getValue(), curr);
+						vertices.add(curr);
+					}
+
+					// add two way edge
+					newWord.addNeighbor(curr);
+					curr.addNeighbor(newWord);
+				}
+				valueMap.put(newWord.getValue(), newWord);
+				line = reader.readLine();
+			}
+		} catch (IOException ioException) {
+			// TODO: do something
+		}
+	}
+
+	private void loadDictionary(File file) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
+
+			// read the words
+			String word = reader.readLine();
+			while (word != null) {
+				// construct word node
+				word = word.replaceAll("[-+'.^:,]","");
+				GraphNode<String> newWord = new GraphNode<>(word.toLowerCase());
+
+				// add appropriate edges using the edge function
+				int currentGraphSize = vertices.size();
+				for (int i = 0; i < currentGraphSize; i++) {
+					GraphNode<String> currentWord = vertices.get(i);
+
+					if (onlyOneChange(currentWord.getValue(), newWord.getValue())) {
+						currentWord.addNeighbor(newWord);
+						newWord.addNeighbor(currentWord);
+					}
+				}
+
+				// add new word to overall graph
+				valueMap.put(newWord.getValue(), newWord);
+				vertices.add(newWord);
+
+				// next word
+				word = reader.readLine();
+			}
+
+			// clean up
+			reader.close();
+
+		} catch (FileNotFoundException fileNotFoundException) {
+			// TODO: do something
+		} catch (IOException ioException) {
+			// TODO: do something
+		}
+
+		prune();
+	}
+
+	WordGraph(File file, boolean isGraphFile) {
+		if (isGraphFile) {
+			restoreGraphFromFile(file);
+		} else {
+			loadDictionary(file);
+		}
+	}
+
+	ArrayList<GraphNode<String>> getRandomPath(int pathSize) {
+		if (isEmpty()) {
+			return null;
+		}
+
+		ArrayList<GraphNode<String>> path = null;
+
+		while (path == null) {
+			path = getRandomPathHelper(pathSize);
+		}
+
+		return path;
+	}
+
+	private ArrayList<GraphNode<String>> getRandomPathHelper(int pathSize) {
+		HashSet<String> uniqueWords = new HashSet<>();
+		ArrayList<GraphNode<String>> path = new ArrayList<>();
+		GraphNode<String> vertex = getRandomVertex();
+
+		path.add(vertex);
+		uniqueWords.add(vertex.toString());
+
+		while (path.size() < pathSize) {
+			GraphNode<String> next = vertex.getRandomNeighbor();
+			if (uniqueWords.contains(next.toString())) {
+				return null;
+			} else {
+				uniqueWords.add(next.toString());
+				path.add(next);
+			}
+			vertex = next;
+		}
+
+		return path;
+	}
+
+	// edge function for WordGraph
+	private boolean onlyOneChange(String aWord, String bWord) {
+		if (aWord == null || bWord == null) {
+			return false;
+		}
+
+		if (aWord.length() != bWord.length()) {
+			return false;
+		}
+
+		int changeCount = 0;
+		for (int i = 0; i < aWord.length(); i++) {
+			char aChar = aWord.charAt(i);
+			char bChar = bWord.charAt(i);
+
+			if (aChar != bChar)  {
+				changeCount++;
+			}
+		}
+
+		return changeCount == 1;
+	}
+
+}
