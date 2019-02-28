5e03ece6eba531abeaa63d1ccef26ccaa4471a32
p-kim1 <pkim1@fandm.edu>
2019-02-28 17:18:32 -0500

    Implemented hint image methods.

--- app/src/main/java/com/example/mobileapp/wordly/inGame.java
+++ app/src/main/java/com/example/mobileapp/wordly/inGame.java
@@ -1,6 +1,8 @@
 package com.example.mobileapp.wordly;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -10,12 +12,22 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.net.ssl.HttpsURLConnection;
 
 public class inGame extends AppCompatActivity {
@@ -24,317 +36,98 @@ public class inGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
-    }
-}
-
-public class NetworkFragment extends Fragment {
-    public static final String TAG = "NetworkFragment";
-    private static final String URL_KEY = "UrlKey";
-    private NetworkActivity.DownloadCallback<String> mCallback;
-    private DownloadTask downloadTask;
-    private String urlString;
-
-    /**
-     * Static initializer for NetworkFragment that sets the URL of the host it will be downloading
-     * from.
-     */
-    public static NetworkFragment getInstance(FragmentManager fragmentManager, String url) {
-        NetworkFragment networkFragment = new NetworkFragment();
-        Bundle args = new Bundle();
-        args.putString(URL_KEY, url);
-        networkFragment.setArguments(args);
-        fragmentManager.beginTransaction().add(networkFragment, TAG).commit();
-        return networkFragment;
+        //getHintImage("pizza");
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        urlString = getArguments().getString(URL_KEY);
+    public void getHintImage(String word)
+    {
+        String URLstr = "https://pixabay.com/images/search/" + word;
+        getURL search = new getURL();
+        search.execute(URLstr);
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        // Host Activity will handle callbacks from task.
-        mCallback = (NetworkActivity.DownloadCallback<String>) context;
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        // Clear reference to host Activity to avoid memory leak.
-        mCallback = null;
-    }
-
-    @Override
-    public void onDestroy() {
-        // Cancel task when Fragment is destroyed.
-        cancelDownload();
-        super.onDestroy();
-    }
-
-    /**
-     * Start non-blocking execution of DownloadTask.
-     */
-    public void startDownload() {
-        cancelDownload();
-        downloadTask = new DownloadTask(mCallback);
-        downloadTask.execute(urlString);
-    }
-
-    /**
-     * Cancel (and interrupt if necessary) any ongoing DownloadTask execution.
-     */
-    public void cancelDownload() {
-        if (downloadTask != null) {
-            downloadTask.cancel(true);
-        }
-    }
-
-    private class DownloadTask extends AsyncTask<String, Integer, DownloadTask.Result> {
-
-        private NetworkActivity.DownloadCallback<String> mCallback;
-
-        DownloadTask(NetworkActivity.DownloadCallback<String> callback) {
-            setCallback(callback);
-        }
-
-        void setCallback(NetworkActivity.DownloadCallback<String> callback) {
-            mCallback = callback;
-        }
-
-        /**
-         * Wrapper class that serves as a union of a result value and an exception. When the download
-         * task has completed, either the result value or exception can be a non-null value.
-         * This allows you to pass exceptions to the UI thread that were thrown during doInBackground().
-         */
-        public class Result {
-            public String mResultValue;
-            public Exception mException;
-            public Result(String resultValue) {
-                mResultValue = resultValue;
-            }
-            public Result(Exception exception) {
-                mException = exception;
-            }
-        }
-
-        /**
-         * Cancel background network operation if we do not have network connectivity.
-         */
-        private String downloadUrl(URL url) throws IOException {
-            InputStream stream = null;
-            HttpsURLConnection connection = null;
-            String result = null;
+    public class getURL extends AsyncTask<String, Void, Void> {
+        protected Void doInBackground(String... urlstring)
+        {
             try {
-                connection = (HttpsURLConnection) url.openConnection();
-                // Timeout for reading InputStream arbitrarily set to 3000ms.
-                connection.setReadTimeout(3000);
-                // Timeout for connection.connect() arbitrarily set to 3000ms.
-                connection.setConnectTimeout(3000);
-                // For this use case, set HTTP method to GET.
-                connection.setRequestMethod("GET");
-                // Already true by default but setting just in case; needs to be true since this request
-                // is carrying an input (response) body.
-                connection.setDoInput(true);
-                // Open communications link (network traffic occurs here).
-                connection.connect();
-                publishProgress(NetworkActivity.DownloadCallback.Progress.CONNECT_SUCCESS);
-                int responseCode = connection.getResponseCode();
-                if (responseCode != HttpsURLConnection.HTTP_OK) {
-                    throw new IOException("HTTP error code: " + responseCode);
-                }
-                // Retrieve the response body as an InputStream.
-                stream = connection.getInputStream();
-                publishProgress(NetworkActivity.DownloadCallback.Progress.GET_INPUT_STREAM_SUCCESS, 0);
-                if (stream != null) {
-                    // Converts Stream to String with max length of 500.
-                    result = readStream(stream, 500);
-                }
-            } finally {
-                // Close Stream and disconnect HTTPS connection.
-                if (stream != null) {
-                    stream.close();
-                }
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-            return result;
-        }
-
-        public String readStream(InputStream stream, int maxReadSize)
-                throws IOException, UnsupportedEncodingException {
-            Reader reader = null;
-            reader = new InputStreamReader(stream, "UTF-8");
-            char[] rawBuffer = new char[maxReadSize];
-            int readSize;
-            StringBuffer buffer = new StringBuffer();
-            while (((readSize = reader.read(rawBuffer)) != -1) && maxReadSize > 0) {
-                if (readSize > maxReadSize) {
-                    readSize = maxReadSize;
-                }
-                buffer.append(rawBuffer, 0, readSize);
-                maxReadSize -= readSize;
-            }
-            return buffer.toString();
-        }
-
-
-        @Override
-        protected void onPreExecute() {
-            if (mCallback != null) {
-                NetworkInfo networkInfo = mCallback.getActiveNetworkInfo();
-                if (networkInfo == null || !networkInfo.isConnected() ||
-                        (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
-                                && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
-                    // If no connectivity, cancel task and update Callback with null data.
-                    mCallback.updateFromDownload(null);
-                    cancel(true);
-                }
-            }
-        }
-
-        /**
-         * Defines work to perform on the background thread.
-         */
-        @Override
-        protected DownloadTask.Result doInBackground(String... urls) {
-            Result result = null;
-            if (!isCancelled() && urls != null && urls.length > 0) {
-                String urlString = urls[0];
-                try {
-                    URL url = new URL(urlString);
-                    String resultString = downloadUrl(url);
-                    if (resultString != null) {
-                        result = new Result(resultString);
-                    } else {
-                        throw new IOException("No response received.");
+                URL url = new URL(urlstring[0]);
+                HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+                ArrayList<String> imageURLs = new ArrayList<>();
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String inputLine, imageLink;
+                while ((inputLine = in.readLine()) != null) {
+                    if (inputLine.contains("Related Images")) {
+                        int startIndex = inputLine.indexOf("https");
+                        int endIndex = Math.min(inputLine.indexOf(".jpg"),inputLine.indexOf(".png"));
+                        while (endIndex >= 0) {
+                            imageLink = inputLine.substring(startIndex,endIndex+4);
+                            if(imageLink.contains("480."))
+                                imageURLs.add(imageLink);
+                            startIndex = inputLine.indexOf("https", startIndex + 1);
+                            endIndex = Math.min(inputLine.indexOf(".jpg", endIndex + 1),inputLine.indexOf(".png", endIndex + 1));
+                        }
+                        break;
                     }
-                } catch(Exception e) {
-                    result = new Result(e);
                 }
+                in.close();
+                Random r = new Random();
+                String randomImageURL = imageURLs.get(r.nextInt(imageURLs.size()));
+                setImage st = new setImage();
+                System.out.println(randomImageURL);
+                st.execute(randomImageURL);
             }
-            return result;
+            catch(MalformedURLException e)
+            {
+                e.printStackTrace();
+            }
+            catch(IOException io)
+            {
+                io.printStackTrace();
+            }
+            return null;
         }
+    }
 
-        /**
-         * Updates the com.example.mobileapp.wordly.DownloadCallback with the result.
-         */
-        @Override
-        protected void onPostExecute(Result result) {
-            if (result != null && mCallback != null) {
-                if (result.mException != null) {
-                    mCallback.updateFromDownload(result.mException.getMessage());
-                } else if (result.mResultValue != null) {
-                    mCallback.updateFromDownload(result.mResultValue);
+    public class setImage extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... urlstring)
+        {
+            Bitmap img = null;
+            try {
+                URL url = new URL(urlstring[0]);
+                HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                InputStream in = new BufferedInputStream(url.openStream());
+
+                byte[] b = new byte[1024];
+                int n  = 0;
+                while(-1!=(n=in.read(b)))
+                {
+                    out.write(b,0,n);
                 }
-                mCallback.finishDownloading();
+                out.close();
+                in.close();
+                byte[] r = out.toByteArray();
+                img = BitmapFactory.decodeByteArray(r, 0, r.length);
+
             }
+            catch(MalformedURLException e)
+            {
+                e.printStackTrace();
+            }
+            catch(IOException io)
+            {
+                io.printStackTrace();
+            }
+            return img;
         }
-
-        /**
-         * Override to add special behavior for cancelled AsyncTask.
-         */
-        @Override
-        protected void onCancelled(Result result) {
-        }
-    }
-
-
-}
-
-
-public class NetworkActivity extends FragmentActivity implements NetworkActivity.DownloadCallback {
-    private NetworkFragment networkFragment;
-    private boolean downloading = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        networkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://www.google.com");
-    }
-
-    private void startDownload() {
-        if (!downloading && networkFragment != null) {
-            // Execute the async download.
-            networkFragment.startDownload();
-            downloading = true;
+        protected void onPostExecute(Bitmap out)
+        {
+            ImageView iv = (ImageView)findViewById(R.id.hint_image);
+            iv.setImageBitmap(out);
         }
     }
-
-
-
-    @Override
-    public void updateFromDownload(String result) {
-        System.out.println("test");
-        // Update your UI here based on result of download.
-    }
-
-    @Override
-    public NetworkInfo getActiveNetworkInfo() {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        return networkInfo;
-    }
-
-    @Override
-    public void onProgressUpdate(int progressCode, int percentComplete) {
-        switch(progressCode) {
-            // You can add UI behavior for progress updates here.
-            case DownloadCallback.Progress.ERROR:
-                break;
-            case DownloadCallback.Progress.CONNECT_SUCCESS:
-                break;
-            case DownloadCallback.Progress.GET_INPUT_STREAM_SUCCESS:
-                break;
-            case DownloadCallback.Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
-                break;
-            case DownloadCallback.Progress.PROCESS_INPUT_STREAM_SUCCESS:
-                break;
-        }
-    }
-
-    @Override
-    public void finishDownloading() {
-        downloading = false;
-        if (networkFragment != null) {
-            networkFragment.cancelDownload();
-        }
-    }
-    interface DownloadCallback<T> {
-        interface Progress {
-            int ERROR = -1;
-            int CONNECT_SUCCESS = 0;
-            int GET_INPUT_STREAM_SUCCESS = 1;
-            int PROCESS_INPUT_STREAM_IN_PROGRESS = 2;
-            int PROCESS_INPUT_STREAM_SUCCESS = 3;
-        }
-
-        /**
-         * Indicates that the callback handler needs to update its appearance or information based on
-         * the result of the task. Expected to be called from the main thread.
-         */
-        void updateFromDownload(T result);
-
-        /**
-         * Get the device's active network status in the form of a NetworkInfo object.
-         */
-        NetworkInfo getActiveNetworkInfo();
-
-        /**
-         * Indicate to callback handler any progress update.
-         * @param progressCode must be one of the constants defined in com.example.mobileapp.wordly.DownloadCallback.Progress.
-         * @param percentComplete must be 0-100.
-         */
-        void onProgressUpdate(int progressCode, int percentComplete);
-
-        /**
-         * Indicates that the download operation has finished. This method is called even if the
-         * download hasn't completed successfully.
-         */
-        void finishDownloading();
-    }
-
 }
