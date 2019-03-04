6a2fa5fbd478453c72b79e5fd0673095b505e2f9
p-kim1 <pkim1@fandm.edu>
2019-03-03 22:27:10 -0500

    Implemented hint image methods.

--- app/src/main/java/com/example/mobileapp/wordly/inGame.java
+++ app/src/main/java/com/example/mobileapp/wordly/inGame.java
@@ -13,7 +13,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -27,16 +35,25 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class inGame extends AppCompatActivity {
 
+    private static Context appContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
-        //getHintImage("pizza");
+        appContext = getApplicationContext();
+        getHintImage("pizza");
+
+        //WordGame game = new WordGame(appContext,4);
+        //System.out.println(game.getCurrentPath());
     }
 
     public void getHintImage(String word)
@@ -46,37 +63,46 @@ public class inGame extends AppCompatActivity {
         search.execute(URLstr);
     }
 
-    public class getURL extends AsyncTask<String, Void, Void> {
-        protected Void doInBackground(String... urlstring)
+    public class getURL extends AsyncTask<String, Void, ArrayList<String>> {
+        protected void onPreExecute()
         {
+            TextView tv = (TextView) findViewById(R.id.inGame_TextView_loading);
+            Animation a = AnimationUtils.loadAnimation(appContext, R.anim.blink);
+            tv.setAnimation(a);
+            tv.animate();
+        }
+
+        protected ArrayList<String> doInBackground(String... urlstring)
+        {
+            ArrayList<String> imageURLs = null;
             try {
                 URL url = new URL(urlstring[0]);
                 HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
                 con.connect();
-                ArrayList<String> imageURLs = new ArrayList<>();
+                imageURLs = new ArrayList<>();
                 BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                 String inputLine, imageLink;
                 while ((inputLine = in.readLine()) != null) {
                     if (inputLine.contains("Related Images")) {
                         int startIndex = inputLine.indexOf("https");
-                        int endIndex = Math.min(inputLine.indexOf(".jpg"),inputLine.indexOf(".png"));
+                        int endIndex = Math.min(inputLine.indexOf(".jpg"), inputLine.indexOf(".png"));
+                        if(endIndex == -1)
+                            endIndex = Math.max(inputLine.indexOf(".jpg"), inputLine.indexOf(".png"));
                         while (endIndex >= 0) {
-                            imageLink = inputLine.substring(startIndex,endIndex+4);
+                            imageLink = inputLine.substring(startIndex,endIndex + 4);
                             if(imageLink.contains("480."))
                                 imageURLs.add(imageLink);
                             startIndex = inputLine.indexOf("https", startIndex + 1);
-                            endIndex = Math.min(inputLine.indexOf(".jpg", endIndex + 1),inputLine.indexOf(".png", endIndex + 1));
+                            if(inputLine.contains(".png"))
+                                endIndex = Math.min(inputLine.indexOf(".jpg", endIndex + 1), inputLine.indexOf(".png", endIndex+1));
+                            else
+                                endIndex = inputLine.indexOf(".jpg", endIndex+1);
                         }
                         break;
                     }
                 }
                 in.close();
-                Random r = new Random();
-                String randomImageURL = imageURLs.get(r.nextInt(imageURLs.size()));
-                setImage st = new setImage();
-                System.out.println(randomImageURL);
-                st.execute(randomImageURL);
             }
             catch(MalformedURLException e)
             {
@@ -86,13 +112,28 @@ public class inGame extends AppCompatActivity {
             {
                 io.printStackTrace();
             }
-            return null;
+            return imageURLs;
+        }
+        protected void onPostExecute(final ArrayList<String> imageURLs)
+        {
+            TimerTask repeatImage = new TimerTask() {
+                int i = 0;
+                @Override
+                public void run() {
+                    if(i == imageURLs.size())
+                        i = 0;
+                    setImage st = new setImage();
+                    st.execute(imageURLs.get(i));
+                    i++;
+                }
+            };
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(repeatImage, 0, 4000);
         }
     }
 
     public class setImage extends AsyncTask<String, Void, Bitmap> {
-        protected Bitmap doInBackground(String... urlstring)
-        {
+        protected Bitmap doInBackground(String... urlstring) {
             Bitmap img = null;
             try {
                 URL url = new URL(urlstring[0]);
@@ -103,31 +144,34 @@ public class inGame extends AppCompatActivity {
                 InputStream in = new BufferedInputStream(url.openStream());
 
                 byte[] b = new byte[1024];
-                int n  = 0;
-                while(-1!=(n=in.read(b)))
-                {
-                    out.write(b,0,n);
+                int n = 0;
+                while (-1 != (n = in.read(b))) {
+                    out.write(b, 0, n);
                 }
                 out.close();
                 in.close();
                 byte[] r = out.toByteArray();
                 img = BitmapFactory.decodeByteArray(r, 0, r.length);
 
-            }
-            catch(MalformedURLException e)
-            {
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
-            }
-            catch(IOException io)
-            {
+            } catch (IOException io) {
                 io.printStackTrace();
             }
             return img;
         }
-        protected void onPostExecute(Bitmap out)
-        {
-            ImageView iv = (ImageView)findViewById(R.id.hint_image);
+
+        protected void onPostExecute(Bitmap out) {
+            TextView tv = (TextView) findViewById(R.id.inGame_TextView_loading);
+            tv.setVisibility(View.INVISIBLE);
+            tv.clearAnimation();
+            ImageView iv = (ImageView) findViewById(R.id.inGame_ImageView_hint);
             iv.setImageBitmap(out);
+
+            iv.setVisibility(View.VISIBLE);
+            Animation a = AnimationUtils.loadAnimation(appContext, R.anim.fade_in);
+            iv.setAnimation(a);
+            iv.animate();
         }
     }
 }
